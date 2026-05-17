afe11678387da569612dd1ce19bb83a3f93f7409
alex-smitg <cprogramming78@gmail.com>
2026-05-17 21:11:26 +0300

    Add drawList function for assets

--- src/editor/user_interface/assets_window.h
+++ src/editor/user_interface/assets_window.h
@@ -2,6 +2,7 @@
 
 #include <string>
 #include <filesystem>
+#include <typeinfo>
 
 #include "imgui_docking/imgui.h"
 #include "imgui_docking/imgui_impl_glfw.h"
@@ -25,13 +26,53 @@ public:
 		this->assetRepository = assetRepository;
 	}
 
+	template <typename T>
+	void drawList(std::string name, std::string payloadName, std::vector<AssetSlot<T>*> slots,
+		std::function<void(int)> deleteFunction, std::function<void()> createFunction) {
+		if (ImGui::TreeNodeEx(name.c_str()))
+		{
+			if (ImGui::Button("+"))
+			{
+				createFunction();
+			}
+			for (const AssetSlot<T>* assetSlot : slots)
+			{
+				ImGui::PushID(assetSlot->index);
+				if (assetSlot->is_valid && ImGui::TreeNodeEx(assetSlot->asset->name.c_str(), ImGuiTreeNodeFlags_Leaf)) {
+					T* asset = assetSlot->asset;
+
+					if (ImGui::BeginPopupContextItem())
+					{
+						ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(1.0, 0.0, 0.0, 1.0));
+						if (ImGui::MenuItem("Delete")) {
+							deleteFunction(assetSlot->index);
+						}
+						ImGui::PopStyleColor();
+						ImGui::EndPopup();
+					}
+
+					if (ImGui::BeginDragDropSource())
+					{
+						ImGui::SetDragDropPayload(payloadName.c_str(), &assetSlot->index, sizeof(assetSlot->index));
+						ImGui::Text(asset->name.c_str());
+						ImGui::EndDragDropSource();
+					}
+
+					ImGui::TreePop();
+				}
+				ImGui::PopID();
+			}
+			ImGui::TreePop();
+		}
+	}
+
 	void draw()
 	{
 		if (open)
 		{
 			ImGui::Begin("Assets", &open);
-			if (ImGui::TreeNodeEx("Textures"))
-			{
+			//if (ImGui::TreeNodeEx("Textures"))
+			//{
 			//	for (auto const &pair : assetRepository->texturesMap)
 			//	{
 			//		std::shared_ptr<Texture> texture = pair.second;
@@ -64,174 +105,71 @@ public:
 			//		}*/
 			//	}
 
-				ImGui::TreePop();
-			}
+			//	ImGui::TreePop();
+			//}
+			drawList<Vertices>(
+				"Vertices",
+				"VERTICES",
+				assetRepository->vertices.slots,
+				[this](int index) {
+					assetRepository->vertices.remove(index);
+				},
+				[this]() {
+					std::filesystem::path objFilePath = drishengine::openModelOpenFileDialog();
 
-			if (ImGui::TreeNodeEx("Vertices"))
-			{
-				//for (auto const &pair : assetRepository->verticesMap)
-				//{
-				//	std::shared_ptr<Vertices> vertices = pair.second;
-				//	ImGui::Text("assID: %d", vertices->assId);
-				//	ImGui::SameLine();
-				//	ImGui::Spacing();
-				//	ImGui::SameLine();
-				//	ImGui::Text(vertices->name.c_str());
-				//	ImGui::SameLine();
-				//	ImGui::PushID(vertices->assId);
-				//	ImGui::Button("Drag");
-				//	if (ImGui::BeginDragDropSource())
-				//	{
-				//		ImGui::SetDragDropPayload("VERTICES", &vertices->assId, sizeof(vertices->assId));
-				//		ImGui::Text(vertices->name.c_str());
-				//		ImGui::EndDragDropSource();
-				//	}
-				//	ImGui::PopID();
-				//}
-				////if (ImGui::Button("+", ImVec2(64, 64)))
-				////{
-				////	std::filesystem::path objFilePath = drishengine::openModelOpenFileDialog();
+					if (!objFilePath.empty())
+					{
+						std::filesystem::path filename = objFilePath.filename();
+						filename.replace_extension("model");
 
-				////	if (!objFilePath.empty())
-				////	{
-				////		std::filesystem::path filename = objFilePath.filename();
-				////		filename.replace_extension("model");
+						Vertices* vertices = new Vertices();
+						vertices->name = objFilePath.stem().string();
 
-				////		Vertices *vertices = new Vertices(-1);
-				////		vertices->name = objFilePath.stem().string();
+						vertices->path = std::filesystem::path("models") / filename;
+						drishengine::loadObj(objFilePath, vertices);
 
-				////		vertices->path = std::filesystem::path("models") / filename;
-				////		drishengine::loadObj(objFilePath, vertices);
-				////		//assetRepository->addVertices(vertices);
+						assetRepository->vertices.add(vertices);
 
-				////		if (drishPath != nullptr)
-				////		{
-				////			if (std::filesystem::exists(drishPath->parent_path() / "models"))
-				////			{
-				////				logDebug("[ASSETS WINDOW] models exists");
-				////			}
-				////			else
-				////			{
-				////				std::filesystem::create_directory(drishPath->parent_path() / "models");
-				////			}
-
-				////			drishengine::writeModelData(drishPath->parent_path() / "models" / filename,
-				////										vertices->data);
-				////			vertices->createBuffers();
-				////		}
-				////		else
-				////		{
-				////			logError("[ASSETS WINDOW] drishPath is null");
-				////		}
-				////	}
-				////}
-
-				ImGui::TreePop();
-			}
-			if (ImGui::TreeNodeEx("Materials"))
-			{
-				
-				for (const AssetSlot<Material> * assetSlot : assetRepository->materials.slots)
-				{
-					if (assetSlot->is_valid) {
-						Material* material = assetSlot->asset;
-
-
-						ImGui::Text("%d", assetSlot->index);
-						ImGui::SameLine();
-						ImGui::Text(material->name.c_str());
-						ImGui::SameLine();
-						ImGui::PushID(material->assId);
-						ImGui::Button("Drag");
-						if (ImGui::BeginDragDropSource())
+						if (drishPath != nullptr)
 						{
-							ImGui::SetDragDropPayload("MATERIAL", &assetSlot->index, sizeof(assetSlot->index));
-							ImGui::Text("%d. %s", assetSlot->index, material->name.c_str());
-							ImGui::EndDragDropSource();
-						}
-						ImGui::SameLine();
-						if (ImGui::Button("Delete")) {
-							assetRepository->materials.remove(assetSlot->index);
-						}
-						ImGui::PopID();
-					}
-				}
+							if (std::filesystem::exists(drishPath->parent_path() / "models"))
+							{
+								logDebug("[ASSETS WINDOW] models exists");
+							}
+							else
+							{
+								std::filesystem::create_directory(drishPath->parent_path() / "models");
+							}
 
-				if (ImGui::Button("Create new"))
-				{
-					Material* mat = new Material(AUTO_INCREMENT_ID);
+							drishengine::writeModelData(drishPath->parent_path() / "models" / filename,
+								vertices->data);
+							vertices->createBuffers();
+						}
+						else
+						{
+							logError("[ASSETS WINDOW] drishPath is null");
+						}
+						}
+					}
+				);
+
+
+			drawList<Material>(
+				"Materials",
+				"MATERIAL",
+				assetRepository->materials.slots,
+				[this](int index) {
+					assetRepository->materials.remove(index);
+				},
+				[this]() {
+					Material* mat = new Material();
 					mat->name = "New material";
 					mat->shader = &assetRepository->defaultShader;
 					assetRepository->materials.add(mat);
-					
 				}
+			);
 
-				ImGui::TreePop();
-			}
-
-			if (ImGui::TreeNodeEx("Scripts"))
-			{
-				//for (auto const &pair : assetRepository->scriptsMap)
-				//{
-				//	std::shared_ptr<Script> script = pair.second;
-				//	ImGui::Text("assID: %d", script->assId);
-				//	ImGui::SameLine();
-				//	ImGui::Spacing();
-				//	ImGui::SameLine();
-				//	ImGui::Text(script->name.c_str());
-				//	ImGui::SameLine();
-				//	ImGui::PushID(script->assId);
-				//	ImGui::Button("Drag");
-				//	if (ImGui::BeginDragDropSource())
-				//	{
-				//		ImGui::SetDragDropPayload("SCRIPT", &script->assId, sizeof(script->assId));
-				//		ImGui::Text(script->name.c_str());
-				//		ImGui::EndDragDropSource();
-				//	}
-				//	ImGui::PopID();
-				//}
-
-				///*if (ImGui::Button("+", ImVec2(64, 64)))
-				//{
-				//	Script* script = new Script(AUTO_INCREMENT_ID);
-				//	script->name = "Script";
-				//	assetRepository->addScript(script);
-				//}*/
-
-				ImGui::TreePop();
-			}
-
-			if (ImGui::TreeNodeEx("Sounds"))
-			{
-				/*for (auto const &pair : assetRepository->soundsMap)
-				{
-					std::shared_ptr<Sound> sound = pair.second;
-					ImGui::Text("assID: %d", sound->assId);
-					ImGui::SameLine();
-					ImGui::Spacing();
-					ImGui::SameLine();
-					ImGui::Text(sound->name.c_str());
-					ImGui::SameLine();
-					ImGui::PushID(sound->assId);
-					ImGui::Button("Drag");
-					if (ImGui::BeginDragDropSource())
-					{
-						ImGui::SetDragDropPayload("SOUND", &sound->assId, sizeof(sound->assId));
-						ImGui::Text(sound->name.c_str());
-						ImGui::EndDragDropSource();
-					}
-					ImGui::PopID();
-				}*/
-
-				/*if (ImGui::Button("+", ImVec2(64, 64)))
-				{
-					Sound *sound = new Sound(AUTO_INCREMENT_ID);
-					assetRepository->addSound(sound);
-				}*/
-
-				ImGui::TreePop();
-			}
-
+	
 			ImGui::End();
 		}
 	}
