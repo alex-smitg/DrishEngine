7314a4a3f9fd371aa08ee5fc3a43d739b2a1b2ad
alex-smitg <cprogramming78@gmail.com>
2026-08-15 05:41:05 +0300

    Rename variables and remove unneeded code

--- src/editor/user_interface/assets_window.h
+++ src/editor/user_interface/assets_window.h
@@ -25,6 +25,8 @@ public:
 
 	FsItem* parent = nullptr;
 
+	std::filesystem::path absolutePath;
+
 	unsigned int textureID = -1; // -1 = no texture
 	bool useTexture = true;
 	unsigned int color = 0xFF00FF;
@@ -49,14 +51,12 @@ public:
 };
 
 
-class Folder: public FsItem
+class Directory: public FsItem
 {
 public:
 	std::map<std::string, FsItem*> content;
 
-	
-
-	Folder(std::string name) {
+	Directory(std::string name) {
 		this->name = name;
 	}
 
@@ -73,13 +73,13 @@ public:
 		return file;
 	}
 
-	Folder* addFolder(std::string name) {
-		Folder* folder = new Folder(name);
-		folder->parent = this;
+	Directory* addDirectory(std::string name) {
+		Directory* childDirectory = new Directory(name);
+		childDirectory->parent = this;
 		
-		content[name] = folder;
+		content[name] = childDirectory;
 		
-		return folder;
+		return childDirectory;
 	}
 };
 
@@ -91,124 +91,15 @@ private:
 public:
 	std::filesystem::path *drishPath = nullptr;
 
+	Directory projectRoot = Directory("project");
 
-	
-	
-
-	Folder fs = Folder("project");
-	std::vector<Folder*> currentFolderPath;
-	Folder* selectedFolder = &fs;
+	std::vector<Directory*> currentDirectoryPath;
+	Directory* currentDirectory = &projectRoot;
 
 	AssetWindow(AssetRepository *assetRepository, std::filesystem::path* drishPath)
 	{
 		this->assetRepository = assetRepository;
 		this->drishPath = drishPath;
-	}
-
-	template <typename T>
-	void drawList(std::string name, std::string payloadName, std::vector<AssetSlot<T>*> slots,
-		std::function<void(int)> deleteFunction, std::function<void()> createFunction) {
-
-
-		if (ImGui::TreeNodeEx(name.c_str(), ImGuiTreeNodeFlags_DrawLinesToNodes | ImGuiTreeNodeFlags_DefaultOpen))
-		{
-			if (true) {
-			
-			//commented code is crashing if asset_window is moved outside main window
-			//if (ImGui::BeginChild("##child", ImVec2(-FLT_MIN, ImGui::GetTextLineHeightWithSpacing() * 8), ImGuiChildFlags_Borders | ImGuiChildFlags_ResizeY)) {
-				if (ImGui::Button("+"))
-				{
-					createFunction();
-				}
-				if (ImGui::BeginTable(name.c_str(),
-					2,
-					 ImGuiTableFlags_ScrollY | ImGuiTableFlags_RowBg, ImVec2(0, 32*6))) {
-
-					ImGui::TableSetupColumn("##image", ImGuiTableColumnFlags_WidthFixed, 32.0f);
-					ImGui::TableSetupColumn("##text", 0.0f);
-
-					for (const AssetSlot<T>* assetSlot : slots)
-					{
-						ImGui::PushID(assetSlot->index);
-						if (assetSlot->is_valid) {
-							T* asset = assetSlot->asset;
-
-							ImGui::TableNextRow();
-							ImGui::TableSetColumnIndex(0);
-							if (typeid(T) == typeid(Texture)) {
-								Asset* ass = static_cast<Asset*>(asset);
-								Texture* tex = static_cast<Texture*>(ass);
-								ImGui::Image(tex->glid, ImVec2(32, 32));
-							}
-							ImGui::TableSetColumnIndex(1);
-							ImGui::Selectable(asset->name.c_str(), false, ImGuiSelectableFlags_SpanAllColumns | ImGuiSelectableFlags_AllowOverlap, ImVec2(0, 32));
-							if (ImGui::BeginPopupContextItem())
-							{
-								ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(1.0, 0.0, 0.0, 1.0));
-								if (ImGui::MenuItem("Delete")) {
-									deleteFunction(assetSlot->index);
-								}
-								ImGui::PopStyleColor();
-								ImGui::EndPopup();
-							}
-
-							if (ImGui::BeginDragDropSource())
-							{
-								ImGui::SetDragDropPayload(payloadName.c_str(), &assetSlot->index, sizeof(assetSlot->index));
-								ImGui::Text(asset->name.c_str());
-								ImGui::EndDragDropSource();
-							}
-						}
-						ImGui::PopID();
-					}
-					ImGui::EndTable();
-				}
-				//ImGui::EndChild();	
-			}
-			ImGui::TreePop();
-		}
-	}
-
-	void drawFolderContents(Folder* folder) {
-		ImGuiTreeNodeFlags flags;
-		flags = ImGuiTreeNodeFlags_DrawLinesToNodes | ImGuiTreeNodeFlags_DefaultOpen;
-
-		if (folder->content.empty()) {
-			flags |= ImGuiTreeNodeFlags_Leaf;
-		}
-
-		
-
-		if (selectedFolder == folder) {
-			flags |= ImGuiTreeNodeFlags_Selected;
-			ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(0.0, 1.0, 0.9, 1.0));
-		}
-
-
-		ImGui::SetNextItemOpen(false, ImGuiCond_Once);
-		bool treeOpened = ImGui::TreeNodeEx(folder->name.c_str(), flags);
-
-		if (selectedFolder == folder) {
-			ImGui::PopStyleColor();
-		}
-
-		if (ImGui::IsItemClicked()) {
-			changeCurrentFolder(folder);
-		}
-
-
-		if (treeOpened) {
-
-			for (auto a : folder->content) {
-				FsItem* item = a.second;
-				if (item->isDirectory()) {
-					drawFolderContents((Folder*)item);
-				}
-			}
-			ImGui::TreePop();
-		}
-
-		
 	}
 
 
@@ -246,7 +137,7 @@ public:
 					logInfo("[ASSETS WINDOW] file already exist, no need to copy");
 				}
 				else {
-					std::filesystem::copy_file(texturePath, drishPath->parent_path() / "textures" / filename);
+					std::filesystem::copy_file(texturePath, currentDirectory->absolutePath / filename);
 				}
 			}	
 			else
@@ -259,14 +150,16 @@ public:
 		return nullptr;
 	}
 
-	void changeCurrentFolder(Folder* folder) {
-		selectedFolder = folder;
-		currentFolderPath.clear();
-		Folder* f = selectedFolder;
+	void changeCurrentFolder(Directory* directory) {
+		currentDirectory = directory;
 
-		while (f != nullptr) {
-			currentFolderPath.push_back(f);
-			f = static_cast<Folder*>(f->parent);
+		currentDirectoryPath.clear();
+
+		Directory* tempDirectory = directory;
+
+		while (tempDirectory != nullptr) {
+			currentDirectoryPath.push_back(tempDirectory);
+			tempDirectory = static_cast<Directory*>(tempDirectory->parent);
 		}
 
 	}
@@ -275,7 +168,7 @@ public:
 	void refresh() {
 		std::map<std::string, FsItem*> index;
 
-		index[(drishPath->parent_path() / "project").string()] = &fs;
+		index[(drishPath->parent_path() / "project").string()] = &projectRoot;
 
 		for (const std::filesystem::directory_entry& entry :
 			std::filesystem::recursive_directory_iterator(drishPath->parent_path() / "project")) {
@@ -283,20 +176,23 @@ public:
 			
 			FsItem* parent = index[entry.path().parent_path().string()];
 			if (parent->isDirectory()) {
-				Folder* folder = static_cast<Folder*>(parent);
+				Directory* parentDirectory = static_cast<Directory*>(parent);
 
 				if (entry.is_directory()) {
-					index[entry.path().string()] = folder->addFolder(entry.path().filename().string());
+					Directory* childDirectory = parentDirectory->addDirectory(entry.path().filename().string());
+					index[entry.path().string()] = childDirectory;
+					childDirectory->absolutePath = entry.path().string();
 				}
 				else {
-					File* file = folder->addFile(entry.path().filename().string());
-					index[entry.path().string()] = file;
-					file->useTexture = false;
+					File* childFile = parentDirectory->addFile(entry.path().filename().string());
+					index[entry.path().string()] = childFile;
+					childFile->absolutePath = entry.path().string();
+					childFile->useTexture = false;
 
 					std::string ext = entry.path().extension().string();
 
 					if (ext == ".txt" || ext == ".TXT") {
-						file->color = 0xFF3A3A3A;
+						childFile->color = 0xFF3A3A3A;
 					}
 					
 				}
@@ -311,11 +207,6 @@ public:
 			ImGui::Begin("Assets", &open);
 	
 
-			
-
-			/*ImGui::BeginChild("Tree", { 200, 0 }, ImGuiChildFlags_Borders | ImGuiChildFlags_ResizeX, ImGuiWindowFlags_HorizontalScrollbar);
-			drawFolderContents(&fs);
-			ImGui::EndChild();*/
 			ImGui::SameLine();
 
 			if (ImGui::Button("Refresh")) {
@@ -324,15 +215,15 @@ public:
 
 
 			bool endDisabled = false;
-			if (selectedFolder->parent == nullptr) {
+			if (currentDirectory->parent == nullptr) {
 				ImGui::BeginDisabled();
 				endDisabled = true;
 			}
 
 			
 			if (ImGui::Button("<")) {
-				if (selectedFolder->parent != nullptr) {
-					changeCurrentFolder(static_cast<Folder*>(selectedFolder->parent));
+				if (currentDirectory->parent != nullptr) {
+					changeCurrentFolder(static_cast<Directory*>(currentDirectory->parent));
 				}
 			}
 
@@ -342,17 +233,16 @@ public:
 
 			ImGui::SameLine();
 
-			for (int i = currentFolderPath.size() - 1; i > 0; i--) {
-				if (ImGui::TextLink(currentFolderPath[i]->name.c_str())) {
-					changeCurrentFolder(currentFolderPath[i]);
+			for (int i = currentDirectoryPath.size() - 1; i > 0; i--) {
+				if (ImGui::TextLink(currentDirectoryPath[i]->name.c_str())) {
+					changeCurrentFolder(currentDirectoryPath[i]);
 					break;
 				}
 				ImGui::SameLine();
 				ImGui::Text("/");
 				ImGui::SameLine();
 			}
-			ImGui::Text(selectedFolder->name.c_str());
-			//ImGui::Text(selectedFolderPath.c_str());
+			ImGui::Text(currentDirectory->name.c_str());
 			ImGui::BeginChild("Content", { 0, 0 }, ImGuiChildFlags_Borders);
 
 
@@ -364,15 +254,11 @@ public:
 				isHoveringOverIcon = false;
 			}
 
-			
-
-			//ImVec2 childPos = ImGui::GetWindowPos();
 			int width = ImGui::GetWindowWidth();
 			const int ASSET_SIZE = 48;
 			int r = width / ASSET_SIZE;
 
 			if (r == 0) { r = 1; }
-
 
 			ImGui::PushStyleVar(ImGuiStyleVar_ButtonTextAlign,
 				ImVec2(0.0f, 0.0f));
@@ -382,9 +268,9 @@ public:
 			static FsItem* rightClickedItem = nullptr;
 
 			ImGui::PushStyleVar(ImGuiStyleVar_FramePadding, { 0, 0 });
-			if (selectedFolder != nullptr) {
+			if (currentDirectory != nullptr) {
 				int i = 0;
-				for (auto a : selectedFolder->content) {
+				for (auto a : currentDirectory->content) {
 					if (a.second->isDirectory() == true) {
 						ImGui::PushID(i);
 						if (i % r != 0) {
@@ -392,7 +278,7 @@ public:
 						}
 
 						if (ImGui::Button(a.second->name.c_str(), { ASSET_SIZE, ASSET_SIZE })) {
-							changeCurrentFolder(static_cast<Folder*>(a.second));
+							changeCurrentFolder(static_cast<Directory*>(a.second));
 						}
 
 
@@ -458,7 +344,7 @@ public:
 						renamePopupOpened = true;
 					}
 					if (ImGui::MenuItem("Delete")) {
-						changeCurrentFolder(&fs);
+						changeCurrentFolder(&projectRoot);
 					}
 
 				}
@@ -467,7 +353,7 @@ public:
 						if (ImGui::MenuItem("Textures")) {
 							Texture* texture = importTexture();
 							if (texture != nullptr) {
-								File* file = selectedFolder->addFile(texture->name);
+								File* file = currentDirectory->addFile(texture->name);
 								file->textureID = texture->glid;
 							}
 						}
@@ -482,7 +368,7 @@ public:
 						try
 						{
 							if (std::filesystem::create_directory(drishPath->parent_path() / "New folder")) {
-								Folder* f = selectedFolder->addFolder("new folder");
+								Directory* directory = currentDirectory->addDirectory("new folder");
 							}
 						}
 
@@ -521,10 +407,10 @@ public:
 
 					if (rightClickedItem->parent != nullptr) {
 						if (rightClickedItem->parent->isDirectory()) {
-							Folder* folder = static_cast<Folder*>(rightClickedItem->parent);
-							auto nh = folder->content.extract(rightClickedItem->name);
+							Directory* directory = static_cast<Directory*>(rightClickedItem->parent);
+							auto nh = directory->content.extract(rightClickedItem->name);
 							nh.key() = str;
-							folder->content.insert(std::move(nh));
+							directory->content.insert(std::move(nh));
 
 							rightClickedItem->name = str;
 
